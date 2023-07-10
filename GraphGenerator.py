2833c4f8e3dc52bcfdfeb77aea0855d4c11709b0
AhmetMuratAcar <neistersenat@gmail.com>
2023-07-09 22:32:02 -0700

    Continuing work on generating comparison graphs.

--- GraphGenerator.py
+++ GraphGenerator.py
@@ -1,12 +1,10 @@
 import PIL
-from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
-# font = ImageFont.truetype("/usr/share/fonts/truetype/DejaVuSans.ttf",25)
-img=Image.new("RGBA", (200,200),(0,0,0))
+img = Image.new("RGBA", (200, 200), (0, 0, 0))
 draw = ImageDraw.Draw(img)
-draw.text((0, 0),"This is a test",(255,255,0))
+draw.text((0, 0), "This is a test", (255, 255, 0))
 draw.chord((100, 75, 125, 100), 0, 360, fill='green')
 draw.chord((75, 100, 100, 125), 0, 360, fill='blue')
 draw.chord((125, 125, 150, 150), 0, 360, fill='yellow')
