8b4de08212d7d9ed75c7ee29e30f1e18ca430dc7
AhmetMuratAcar <neistersenat@gmail.com>
2023-09-12 09:48:56 -0700

    Fixing seq_format

--- SequenceClass.py
+++ SequenceClass.py
@@ -25,7 +25,6 @@ class Sequence:
         # Converting U to T
         if "U" in self.totalSeq:
             self.totalSeq = self.totalSeq.replace("U", "T")
-            print(self.totalSeq)
 
     def find_start_stop(self):
         """Finds the start and stop codon of given sequence"""
